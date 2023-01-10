ad0a4539f84314ab87d97061471fd98cc4819a16
Jacek Bakuński <jacek.bakunski@gmail.com>
2023-01-10 16:50:16 +0100

    Fix Navbar

--- infos/urls.py
+++ infos/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import home, contact, about
 
 urlpatterns = [
-    path('', home),
-    path('me', about),
-    path('contact', contact),
+    path('', home, name='home'),
+    path('me', about, name='about'),
+    path('contact', contact, name='contact'),
 ]
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('infos/', permanent=True)),
     path('admin/', admin.site.urls),
     path('infos/', include("infos.urls")),
 ]
