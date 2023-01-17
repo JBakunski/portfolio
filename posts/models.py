74c53d87228723088040163867078affbc0f1d17
Jacek Bakuński <jacek.bakunski@gmail.com>
2023-01-17 11:42:38 +0100

    Create models Post & Author

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True, null=True)
+    created = models.DateTimeField(auto_now=True)
+    modified = models.DateTimeField(auto_now_add=True)
+
+    author = models.ForeignKey(
+        'posts.Author',
+        on_delete=models.CASCADE
+    )
+
+
+class Author(models.Model):
+    nick = models.CharField(max_length=100)
+    email = models.EmailField()
+    bio = models.TextField(blank=True, null=True)
