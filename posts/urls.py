7e5263ea35ad58bdf6073fea52e0e7ceb5859744
Jacek Bakuński <jacek.bakunski@gmail.com>
2023-01-18 13:20:07 +0100

    Add authors

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import posts_list, post_details
+from .views import posts_list, post_details, authors_list, author_details
 
 app_name = 'posts'
 urlpatterns = [
-    path('posts/list/', posts_list, name='list'),
-    path('post/<int:id>', post_details, name='details')
+    path('posts/list/', posts_list, name='posts_list'),
+    path('post/<int:id>', post_details, name='post_details'),
+    path('authors/list', authors_list, name='authors_list'),
+    path('author/<int:id>', author_details, name='author_details')
+
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
-from posts.models import Post
+from posts.models import Post, Author
 
 
 def posts_list(request):
     posts = Post.objects.all()
-    return render(request=request, template_name='posts/list.html', context={'posts': posts, 'title': 'Posts list'})
+    return render(request=request, template_name='posts/posts_list.html', context={'posts': posts, 'title': 'Posts list'})
 
 
 def post_details(request, id):
     post = Post.objects.get(id=id)
-    return render(request=request, template_name='posts/details.html', context={'post': post})
+    return render(request=request, template_name='posts/post_details.html', context={'post': post, 'title': 'Post detail'})
 
 
+def authors_list(request):
+    authors = Author.objects.all()
+    return render(request, template_name='posts/authors_list.html', context={'authors': authors, 'title': 'Authors list'})
 
+
+def author_details(request, id):
+    author = Author.objects.get(id=id)
+    return render(request, template_name='posts/author_details.html', context={'author': author, 'title': 'Author details'})
